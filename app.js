6a71314b9d50ea15e1dd3d01bae3bbce606ce906
Brian <brian.luo@electronet.co.nz>
2026-05-23 23:16:39 +1200

    helmet update

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.use(helmet({
     directives: {
       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
       'img-src': ["'self'", 'data:', 'res.cloudinary.com', 'tile.openstreetmap.org', '*.tile.openstreetmap.org'],
-      'script-src': ["'self'", "'unsafe-inline'"],
+      'script-src': ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
+      'connect-src': ["'self'", 'data:'],
+      'worker-src': ["'self'", 'blob:'],
     }
   }
 }))
